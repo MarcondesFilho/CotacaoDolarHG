875a0834166649026074278596736566034b9d6e
Marcondes Filho <marcondes.filho@imagemseguranca.com.br>
2022-04-07 13:33:10 -0300

    Adicionar arquivos de projeto.

---
+++ CotacaoDolarHG/FrmCotacaoDolar.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Globalization;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace CotacaoDolarHG
+{
+    public partial class FrmCotacaoDolar : Form
+    {
+        public FrmCotacaoDolar()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            UpdateValues();
+            tmrSearch.Enabled = true;
+        }
+
+        private void tmrSearch_Tick(object sender, EventArgs e)
+        {
+            UpdateValues();
+        }
+
+        private void UpdateValues()
+        {
+            string strURL = "https://api.hgbrasil.com/finance?array_limit=1&fields=only_results,USD&key=3f2e6a6a";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    var response = client.GetAsync(strURL).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var result = response.Content.ReadAsStringAsync().Result;
+
+                        Market market = JsonConvert.DeserializeObject<Market>(result);
+
+                        lblBuy.Text = string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", market.Currency.Buy);
+                        lblSell.Text = string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", market.Currency.Sell);
+                        lblVar.Text = string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:P}", market.Currency.Variation / 100);
+                    }
+                    else
+                    {
+                        lblBuy.Text = "-";
+                        lblSell.Text = "-";
+                        lblVar.Text = "-";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    lblBuy.Text = "-";
+                    lblSell.Text = "-";
+                    lblVar.Text = "-";
+
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+        }
+    }
+}
